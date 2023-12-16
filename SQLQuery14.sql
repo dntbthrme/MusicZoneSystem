bca000f22db8fe2f8fd2192096bf39aaf66725e3
Crizel <Crizel@DESKTOP-CTT3QOT>
2023-12-17 00:32:33 +0800

    Update: Can create and read requisition datas

---
+++ SQLQuery14.sql
@@ -0,0 +1,3 @@
+﻿DELETE FROM [dbo].canvas where canvas_id = 4;
+
+SELECT p.prod_name, p.prod_desc, c.canvas_quantity, c.canvas_unit, p.prod_price, c.canvas_total FROM canvas c JOIN product p ON c.prod_id = p.prod_id
